ed3e3531da2310bed8edc2df25648fe6718cbc10
iagovar <iagovar@outlook.com>
2022-12-07 16:59:50 +0100

    Añadida primera versión de herramienta para regresión lineal simple

---
+++ herramientas/herramienta-regresion-lineal-simple/script.js
@@ -0,0 +1,142 @@
+function calcularRegresion() {
+  // Tela de variables
+  const indepVarInput = document.getElementById("varIndependiente");
+  const depVarInput = document.getElementById("varDependiente");
+  const sequences = getSequences(indepVarInput.value, depVarInput.value);
+  const formula = regressionLine(sequences.indepVarSeq, sequences.depVarSeq);
+  const regressionLinePoints = getRegressionLinePoints(
+    formula.slope,
+    formula.yIntercept,
+    sequences.indepVarSeq,
+    sequences.depVarSeq
+    );
+
+  // Escribiendo la formula en el front
+  setFormula(formula.formula);
+
+  // Dibujando los puntos y línea o9en el canvas
+  drawData(
+    sequences.indepVarSeq,
+    sequences.depVarSeq,
+    regressionLinePoints.start,
+    regressionLinePoints.end
+    );
+}
+
+
+// Funciones de apoyo
+
+function getSequences(indepVar, depVar) {
+
+  // Convertir la cadena de texto en una matriz de números
+  const indepVarSeq = indepVar.split(",").map(x => parseInt(x, 10));
+  const depVarSeq = depVar.split(",").map(x => parseInt(x, 10));
+
+  return {"indepVarSeq": indepVarSeq, "depVarSeq": depVarSeq};
+}
+
+function regressionLine(xSeq, ySeq) {
+
+  // Calcular la media de los valores en x y en y
+  const xMean = xSeq.reduce((a, b) => a + b) / xSeq.length;
+  const yMean = ySeq.reduce((a, b) => a + b) / ySeq.length;
+
+  // Calcular el numerador y el denominador de la pendiente
+  let numerator = 0;
+  let denominator = 0;
+  for (let i = 0; i < xSeq.length; i++) {
+    numerator += (xSeq[i] - xMean) * (ySeq[i] - yMean);
+    // AQUÍ es donde ^2 y se normaliza, que te olvidas!!
+    denominator += (xSeq[i] - xMean) ** 2;
+  }
+
+  // Calcular la pendiente y la intersección con el eje y
+  const slope = numerator / denominator;
+  const yIntercept = yMean - slope * xMean;
+
+  // Devolver la fórmula de la recta de regresión en un objeto
+  return {
+    formula: `y = ${slope}x + ${yIntercept}`,
+    slope,
+    yIntercept
+  };
+}
+
+function setFormula(text) {
+  // Obtener el elemento <p> con el ID "formula"
+  const elem = document.getElementById("formula");
+
+  // Establecer el contenido del elemento <p> como la cadena de texto
+  // proporcionada
+  elem.innerHTML = text;
+}
+
+function getRegressionLinePoints(slope, yIntercept, xData, yData) {
+
+  // Queremos alargar la recta para que su punto inicial
+  // y final no coincida con el primer y último datapoint.
+  // Para ello calcularemos la distancia entre el valor 
+  // mínimo y máximo * 10% y aplicaremos este valor a los
+  // puntos mínimo y máximo
+  const maxValue = Math.max(...xData, ...yData);
+  const minValue = Math.min(...xData, ...yData);
+  const distance = maxValue - minValue;
+  const coeficiente = distance * 0.1;
+
+  // Calculamos el primer punto de la recta de regresión
+  // utilizando el valor mínimo de xData como el valor de x
+  // -Entero Para alargar la recta por motivos estéticos
+  const startX = Math.min(...xData) - coeficiente;
+  const startY = slope * startX + yIntercept;
+
+  // Calculamos el último punto de la recta de regresión
+  // utilizando el valor máximo de xData como el valor de x
+  // +Entero Para alargar la recta por motivos estéticos
+  const endX = Math.max(...xData) + coeficiente;
+  const endY = slope * endX + yIntercept;
+
+  // Devolvemos un objeto con los valores de los puntos
+  // de inicio y fin de la recta de regresión
+  return {
+    start: {x: startX, y: startY},
+    end: {x: endX, y: endY}
+  };
+}
+
+function drawData(xData, yData, start, end) {
+
+  // Metemos el canvas como contexto para Chartjs
+  const ctx = document.getElementById('miGrafico').getContext('2d');;
+
+  // Construímos lista de objetos para scatterplot
+  const pointsArray = xData.map((x, i) => ({x: x, y: yData[i]}));
+
+  // Churro de código para chartjs
+  const mixedChart = new Chart(ctx, {
+      type: 'scatter',
+      data: {
+          datasets: [{
+              label: 'Puntos',
+              data: pointsArray,
+              // this dataset is drawn below
+              order: 1,
+              pointBackgroundColor: "Red",
+              backgroundColor: "Red"
+          }, {
+              label: 'Regresión',
+              data: [start, end],
+              type: 'line',
+              // this dataset is drawn on top
+              order: 2,
+              backgroundColor: "Blue",
+              fill: false,
+              borderColor: "Blue",
+              pointRadius: 0
+          }]
+      },
+      options: {
+        // Vacio
+      }
+  });
+}
+
