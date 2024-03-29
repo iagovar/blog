94e9a01365ee422bc98923ac6acf53b9b61f33ac
iagovar <iagovar@outlook.com>
2024-03-29 12:45:39 +0100

    Update on pivot table megatenedores script

--- herramientas/megatenedores-consolidado/js-pivot-table/initialize.js
+++ herramientas/megatenedores-consolidado/js-pivot-table/initialize.js
@@ -1,13 +1,20 @@
 async function initializePivotTable(webPivotTableUrl, tsvUrl) {
+    // Select content container and create a wrapper
+    const contentContainer = document.querySelector('#content');
+    const pivotTableWrapper = document.createElement('div');
+    pivotTableWrapper.id = 'pivot-table-wrapper';
+    pivotTableWrapper.style.width = '100%';
+    pivotTableWrapper.style.height = '70vh';
+    contentContainer.appendChild(pivotTableWrapper);
+
     // Create pivot table element
     const pivotTableElement = document.createElement('web-pivot-table');
-    pivotTableElement.style.width = '100%';
-    pivotTableElement.style.height = '70vh';
+    pivotTableWrapper.appendChild(pivotTableElement);
 
     // Load script into DOM and wait for 2s (no better solution at the moment)
     const pivotTableScript = document.createElement('script');
     pivotTableScript.src = webPivotTableUrl;
-    document.body.appendChild(pivotTableScript);
+    contentContainer.appendChild(pivotTableScript);
     await new Promise(resolve => setTimeout(resolve, 2000));
 
     // Load TSV
