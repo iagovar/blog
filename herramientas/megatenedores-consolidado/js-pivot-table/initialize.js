674344512180cf99dc96bd69e168111751a72bae
iagovar <iagovar@outlook.com>
2024-03-29 10:54:01 +0100

    Script de carga de megatenedores

---
+++ herramientas/megatenedores-consolidado/js-pivot-table/initialize.js
@@ -0,0 +1,16 @@
+export default async function initializePivotTable(webPivotTableUrl, tsvUrl) {
+    // Create pivot table element
+    const pitvotTableElement = document.createElement('web-pivot-table');
+    pivotTableElement.style.width = '100%';
+    pivotTableElement.style.height = '70vh';
+
+    // Load script into DOM and wait for 2s (no better solution at the moment)
+    const pitvotTableScript = document.createElement('script');
+    pitvotTableScript.src = webPivotTableUrl;
+    document.body.appendChild(pitvotTableScript);
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    // Load TSV
+    pivotTableScript.setWptFromCsvUrl(tsvUrl, "\t");
+ 
+}
